e9e25b073b175e884b354f3972483ddc1d3a53da
Jeroen De Swaef <jeroendeswaef@gmail.com>
2020-06-26 19:04:19 +0200

    Added stack widget and button to switch between pages

--- main.go
+++ main.go
@@ -1,72 +1,90 @@
 package main
 
 import (
+	"errors"
 	"log"
+	"os"
 
-	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 )
 
-func main() {
-	// Initialize GTK without parsing any command line arguments.
-	gtk.Init(nil)
+const appID = "com.github.gotk3.gotk3-examples.glade"
 
-	// Create a new toplevel window, set its title, and connect it to the
-	// "destroy" signal to exit the GTK main loop when it is destroyed.
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
-	win.SetTitle("Simple Example")
-	win.Connect("destroy", func() {
-		gtk.MainQuit()
+func main() {
+	// Create a new application.
+	application, err := gtk.ApplicationNew(appID, glib.APPLICATION_FLAGS_NONE)
+	errorCheck(err)
+
+	data, err := Asset("ui/ui.glade")
+	errorCheck(err)
+
+	// Connect function to application activate event
+	application.Connect("activate", func() {
+		log.Println("application activate")
+
+		// Get the GtkBuilder UI definition in the glade file.
+		builder, err := gtk.BuilderNew()
+		builder.AddFromString(string(data))
+		// builder, err := gtk.BuilderNewFromFile("ui/ui.glade")
+		errorCheck(err)
+
+		// Map the handlers to callback functions, and connect the signals
+		// to the Builder.
+		signals := map[string]interface{}{
+			"on_main_window_destroy": onMainWindowDestroy,
+			"on_button_clicked": func() {
+				log.Println("Clicked!")
+				backgroundStackObj, err := builder.GetObject("background_stack")
+				errorCheck(err)
+
+				backgroundStack := backgroundStackObj.(*gtk.Stack)
+				backgroundStack.SetVisibleChildName("page1")
+			},
+		}
+		builder.ConnectSignals(signals)
+
+		// Get the object with the id of "main_window".
+		obj, err := builder.GetObject("main_window")
+		errorCheck(err)
+
+		// Verify that the object is a pointer to a gtk.ApplicationWindow.
+		win, err := isWindow(obj)
+		errorCheck(err)
+
+		// Show the Window and all of its components.
+		win.Show()
+		application.AddWindow(win)
 	})
 
-	pixbuf, err := gdk.PixbufNewFromFile("0YzJMi-8_400x400.jpg")
-	if err != nil {
-		log.Fatal("Unable to create pixbuf", err)
+	// Connect function to application shutdown event, this is not required.
+	application.Connect("shutdown", func() {
+		log.Println("application shutdown")
+	})
+
+	// Launch the application
+	os.Exit(application.Run(os.Args))
+}
+
+func isWindow(obj glib.IObject) (*gtk.Window, error) {
+	// Make type assertion (as per gtk.go).
+	if win, ok := obj.(*gtk.Window); ok {
+		return win, nil
 	}
+	return nil, errors.New("not a *gtk.Window")
+}
 
-	img, err := gtk.ImageNewFromPixbuf(pixbuf)
-	if err != nil {
-		log.Fatal("Unable to create image", err)
+func errorCheck(e error) {
+	if e != nil {
+		// panic for any errors.
+		log.Panic(e)
 	}
+}
 
-	// l, err := gtk.LabelNew("Hello, gotk3!")
-	// if err != nil {
-	// 	log.Fatal("Unable to create label:", err)
-	// }
-	// button, err := gtk.ButtonNewWithLabel("Click me")
-	// if err != nil {
-	// 	log.Fatal("Unable to create button", err)
-	// }
-
-	menuBar, err := gtk.MenuBarNew()
-	if err != nil {
-		log.Fatal("Unable to create menu bar", err)
-	}
-
-	box, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 10)
-	if err != nil {
-		log.Fatal("Unable to create box", err)
-	}
-
-	// button.Connect("clicked", func() {
-	// 	log.Print("Clicked")
-	// })
-
-	box.Add(menuBar)
-	box.Add(img)
-
-	win.Add(box)
-
-	// Set the default window size.
-	win.SetDefaultSize(400, 400)
-
-	// Recursively show all widgets contained in this window.
-	win.ShowAll()
-
-	// Begin executing the GTK main loop.  This blocks until
-	// gtk.MainQuit() is run.
-	gtk.Main()
+// onMainWindowDestory is the callback that is linked to the
+// on_main_window_destroy handler. It is not required to map this,
+// and is here to simply demo how to hook-up custom callbacks.
+func onMainWindowDestroy() {
+	log.Println("onMainWindowDestroy")
+	os.Exit(0)
 }
